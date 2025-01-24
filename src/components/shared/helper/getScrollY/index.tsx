982f34c0c407037f3147b999768fee6a13e7c43c
AIR <air@MacBook-Air-AIR.local>
2025-01-24 13:37:02 +0300

    #add #optimization

--- src/components/shared/helper/getScrollY/index.tsx
+++ src/components/shared/helper/getScrollY/index.tsx
@@ -3,8 +3,12 @@ const getScrollY = (ref: any, setScrollY: any) => (e: any) => {
   const blockOffset = ref.current.offsetTop + ref.current.offsetHeight;
   const scrollY = bottomPoint - blockOffset;
 
-  if (scrollY < e.currentTarget.outerHeight)
+  if (scrollY < e.currentTarget.outerHeight) {
     setScrollY(scrollY > 0 ? scrollY : 0);
+    ref.current.style.willChange = "transform, opacity";
+  } else {
+    ref.current.style.willChange = "auto";
+  }
 };
 
 export default getScrollY;
